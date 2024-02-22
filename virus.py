77f8e0ad5edc9efc77a7ea6c43f8c3e6acb03654
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-02-22 18:01:19 +0100

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,13 +1,21 @@
-from turtle import *
+import turtle
 
-speed(0)
-color('red')
-bgcolor('black')
-b = 200
+a = 0
+b = 0
 
-while b > 0:
-    left(120)  # Change the angle to create a better spiral
-    forward(b)
-    b -= 1
+turtle.bgcolor("black")
+turtle.speed(0)
+turtle.pencolor("green")
+turtle.penup()
+turtle.goto(0,200)
+turtle. pendown()
 
-mainloop()
+while True:
+    turtle.forward(a)
+    turtle.right(b)
+    a +=3
+    b += 1
+    if b == 200:
+        break
+
+turtle.exitonclick()
