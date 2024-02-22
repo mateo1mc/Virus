0991eb0b01870e3f4ea87acd7e8ce3d135294da1
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-02-22 18:04:36 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -6,7 +6,7 @@ bgcolor('black')
 b = 200
 
 while b > 0:
-    left(120)  # Change the angle to create a better spiral
+    left(120)  # Change the angle to create a better spiral --- try 470
     forward(b)
     b -= 1
 
