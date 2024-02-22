7fe7d506273e43e45d18e25646bebc5a67671457
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-02-22 18:00:50 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,1 +1,13 @@
+from turtle import *
 
+speed(0)
+color('red')
+bgcolor('black')
+b = 200
+
+while b > 0:
+    left(120)  # Change the angle to create a better spiral
+    forward(b)
+    b -= 1
+
+mainloop()
